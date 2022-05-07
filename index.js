5d5c63833d6778c1e8814c438519bb63642e8fa7
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-05-07 17:00:03 +0100

    cleaning

--- index.js
+++ index.js
@@ -1,51 +1,49 @@
 #!/usr/bin/env node
-const fs = require('fs');
-const estraverse = require('estraverse');
+const fs = require("fs");
+const estraverse = require("estraverse");
 
-const TYPE = 'type';
-const InnerNodes = ['apply', 'property'];
+const TYPE = "type";
+const InnerNodes = ["apply", "property"];
 
 function toTerm(tree) {
-    let aux = [];
-    let lastTree = aux;
-    tree = estraverse.traverse(tree, {
-      enter: function(node, _) {
-          lastTree = aux.length? aux[aux.length-1] : aux;
-          if (node[TYPE] === 'word') {
-            lastTree.push(`${node[TYPE]}{${node.name}}`);
-          } else if (node[TYPE] === 'value') {
-            lastTree.push(`${node[TYPE]}{${node.value}}`);
-          } else {
-            aux.push([]);
-          }
-      },
-      leave: function(node) { 
-          if (InnerNodes.includes(node[TYPE])) {
-            lastTree = aux.length? aux[aux.length-1] : aux;
-            
-            let children = `op:${lastTree[0]}, args:[${lastTree.slice(1)}]`;
-            aux.pop();
-            lastTree = aux.length? aux[aux.length-1] : aux;
-            lastTree.push(`${node[TYPE]}(${children})`); 
-          }
-      },
-      keys: {
-        apply: ["operator", "args"],
-        property: ["operator","args"],
-        word: [],
-        value: [],
-        regex: []
-      },
-      fallback: "iteration"
-    });
+  let aux = [];
+  let lastTree = aux;
+  tree = estraverse.traverse(tree, {
+    enter: function (node, _) {
+      lastTree = aux.length ? aux[aux.length - 1] : aux;
+      if (node[TYPE] === "word") {
+        lastTree.push(`${node[TYPE]}{${node.name}}`);
+      } else if (node[TYPE] === "value") {
+        lastTree.push(`${node[TYPE]}{${node.value}}`);
+      } else {
+        aux.push([]);
+      }
+    },
+    leave: function (node) {
+      if (InnerNodes.includes(node[TYPE])) {
+        lastTree = aux.length ? aux[aux.length - 1] : aux;
 
-    return aux;
-  }
+        let children = `op:${lastTree[0]}, args:[${lastTree.slice(1)}]`;
+        aux.pop();
+        lastTree = aux.length ? aux[aux.length - 1] : aux;
+        lastTree.push(`${node[TYPE]}(${children})`);
+      }
+    },
+    keys: {
+      apply: ["operator", "args"],
+      property: ["operator", "args"],
+      word: [],
+      value: [],
+      regex: [],
+    },
+    fallback: "iteration",
+  });
 
-  const fileName = process.argv[2] || 'examples/number.json';
-  let ast = JSON.parse(fs.readFileSync(fileName, 'utf8'));
+  return aux;
+}
 
-  let t = toTerm(ast);
-  //let s = JSON.stringify(t, null, 2);
-  //console.log(s);
-  console.log(t[0]);
+const fileName = process.argv[2] || "examples/number.json";
+let ast = JSON.parse(fs.readFileSync(fileName, "utf8"));
+
+let t = toTerm(ast);
+console.log(t[0]);
